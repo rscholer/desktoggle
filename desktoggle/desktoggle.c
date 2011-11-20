590d951aaa91c37eea4c62badc9bb5bbfc6871c8
Raphael Scholer <rscholer@gmx.de>
2011-11-20 20:36:19 +0100

    Add version and help information

--- desktoggle/desktoggle.c
+++ desktoggle/desktoggle.c
@@ -21,12 +21,24 @@ THE SOFTWARE.*/
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 
+#include <getopt.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUG_EMAIL_ADDRESS "rscholer@gmx.de\0"
+#define NAME "Desk Toggle\0"
+#define MAINTAINER "Raphael Scholer\0"
+#define VERSION "0.1\0"
+
 int main(int argc, char *argv[]);
+void output_help(char name[]);
+void output_version(void);
 
 int main(int argc, char *argv[]) {
+	/**************************************************************************
+	 * Declare and Initialize Variables
+	 *************************************************************************/
 	Display *d;
 	Window root;
 	Atom _NET_SHOWING_DESKTOP;
@@ -37,6 +49,47 @@ int main(int argc, char *argv[]) {
 	unsigned long nitems;
 	unsigned long after;
 	unsigned char *data = NULL;
+
+	/* Variables for parsing arguments */
+	int current_option = 0;
+	int option_index = 0;
+	int help_flag = False;
+	int version_flag = False;
+
+	const struct option long_options[] = {
+		{"help",	no_argument,	&help_flag,		True},
+		{"version",	no_argument,	&version_flag,	True},
+		{0,			0,				0,				0}
+	};
+
+	/**************************************************************************
+	 * Parse arguments
+	 *************************************************************************/
+	while (1) {
+		option_index = 0;
+
+		current_option = getopt_long(argc, argv, "", long_options,
+										&option_index);
+
+		if (current_option == -1) {
+			break;
+		}
+	}
+
+	/* Helpful output */
+	/* help_flag takes precendence over version_flag */
+	if ( help_flag == True) {
+		output_help(argv[0]);
+		exit(EXIT_SUCCESS);
+	}
+	else if ( version_flag == True) {
+		output_version();
+		exit(EXIT_SUCCESS);
+	}
+
+	/**************************************************************************
+	 *  Main program
+	 *************************************************************************/
 
 	/* Open the default display */
 	if (!(d = XOpenDisplay(NULL))) {
@@ -93,4 +146,44 @@ int main(int argc, char *argv[]) {
 
 	XCloseDisplay(d);
 	exit(EXIT_SUCCESS);
+}
+
+void output_help(char name[]) {
+	printf("Usage: %s [OPTION]...\n", name);
+	printf("\nOptions:\n");
+	printf("  --help\t\tshow this help message and exit\n");
+	printf("  --version\t\tshow version information and exit\n");
+	printf("\nPlease report %s bugs to <%s>.\n",NAME ,BUG_EMAIL_ADDRESS);
+}
+
+void output_version(void) {
+	printf("%s %s\n", NAME, VERSION);
+	printf("Copyright (C) 2011 %s\n", MAINTAINER);
+	printf("\nPermission is hereby granted, free of charge, to any person ");
+	printf("obtaining a copy\n");
+	printf("of this software and associated documentation files ");
+	printf("(the \"Software\"), to deal\n");
+	printf("in the Software without restriction, including without ");
+	printf("limitation the rights\n");
+	printf("to use, copy, modify, merge, publish, distribute, sublicense, ");
+	printf("and/or sell\n");
+	printf("copies of the Software, and to permit persons to whom the ");
+	printf("Software is\n");
+	printf("furnished to do so, subject to the following conditions:\n");
+	printf("\nThe above copyright notice and this permission notice shall be ");
+	printf("included in\n");
+	printf("all copies or substantial portions of the Software.\n");
+	printf("\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ");
+	printf("ANY KIND, EXPRESS OR\n");
+	printf("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES ");
+	printf("OF MERCHANTABILITY,\n");
+	printf("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. ");
+	printf("IN NO EVENT SHALL THE\n");
+	printf("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,");
+	printf("DAMAGES OR OTHER\n");
+	printf("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR ");
+	printf("OTHERWISE, ARISING FROM,\n");
+	printf("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR ");
+	printf("OTHER DEALINGS IN\n");
+	printf("THE SOFTWARE.\n");
 }
