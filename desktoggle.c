036097a7d48edcf8cf1d3bab32b32b8251dc32a8
Raphael Scholer <rscholer@gmx.de>
2013-11-27 12:30:26 +0100

    Release 1.3.7

--- desktoggle.c
+++ desktoggle.c
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #define PACKAGE_BUGREPORT "https://github.com/rscholer/desktoggle/issues/"
 #define PACKAGE_NAME "desktoggle"
 #define PACKAGE_URL "https://github.com/rscholer/desktoggle/"
-#define VERSION "1.3.6"
+#define VERSION "1.3.7"
 
 const char *PROGRAM_NAME = NULL;
 
