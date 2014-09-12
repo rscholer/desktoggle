12db59d69c9ed018bfaebc86405f5bea5e5d6bef
Raphael Scholer <rscholer@gmx.de>
2014-09-12 05:35:11 +0200

    Fix wrong prototype

--- desktoggle.c
+++ desktoggle.c
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 static void output_help(const char *name);
 static void output_try_help(const char *name);
-static void output_version();
+static void output_version(void);
 
 
 int main(int argc, char *argv[]) {
