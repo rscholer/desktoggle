914c535c833d896e12e149db8b2987ccb47d6a17
Raphael Scholer <rscholer@gmx.de>
2015-01-25 04:00:58 +0100

    Bump version

--- desktoggle.c
+++ desktoggle.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #define PACKAGE_BUGREPORT "https://github.com/rscholer/desktoggle/issues/"
 #define PACKAGE_NAME "desktoggle"
 #define PACKAGE_URL "https://github.com/rscholer/desktoggle/"
-#define VERSION "1.4"
+#define VERSION "1.5.1"
 
 
 #include <getopt.h>
