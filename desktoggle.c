a79febfdbe2e1c2692933e68bc2a13a966d16f91
Raphael Scholer <rascholer@gmail.com>
2018-09-21 21:36:41 +0200

    Remove unnecessary macro

--- desktoggle.c
+++ desktoggle.c
@@ -14,7 +14,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #define AUTHOR "Raphael Scholer"
-#define CDATE "2011-2018"
 #define PACKAGE_NAME "desktoggle"
 #define VERSION "1.7.1"
 
@@ -176,7 +175,7 @@ static void output_try_help(const char *name) {
 static void output_version(void) {
     /* Print version information. Usually invoked by `argv[0] --version` */
     printf("%s %s\n", PACKAGE_NAME, VERSION);
-    printf("Copyright (C) %s %s\n", CDATE, AUTHOR);
+    printf("Copyright (C) %s %s\n", "2011-2018", AUTHOR);
     puts("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.");
     puts("This is free software: you are free to change and redistribute it.");
     puts("There is NO WARRANTY, to the extent permitted by law.");
